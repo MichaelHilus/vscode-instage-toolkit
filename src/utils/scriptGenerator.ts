0dc103bfb983902b464954c2417dcf921f94aa0e
Michael Hilus <m.hilus@progresso-group.de>
2018-06-22 15:56:46 +0200

    Apply colors to borders and backgrounds in generator

--- src/utils/scriptGenerator.ts
+++ src/utils/scriptGenerator.ts
@@ -2,87 +2,38 @@ import { VariableType } from "../models/variableType";
 import { PlantUmlNode } from "../models/plantUml";
 import { Graph, GraphNode } from "../models/graph";
 import { Context } from "../models/context";
-import { Pattern, MatchOperationItem, ReturnOperationItem, CreateOperationItem, DeleteOperationItem } from "../models/pattern";
+import { Pattern, MatchOperationItem, ReturnOperationItem, CreateOperationItem, DeleteOperationItem, OperationItem, RelationIndex, Operations } from "../models/pattern";
 
 
 export function generatePlantUmlScript(variableType: VariableType, variable: any): string {
     let plantUmlNodes: PlantUmlNode[] = [];
     switch(variableType) {
         case VariableType.Graph: {
-            let graphVariable = variable as Graph;
+            const graphVariable = variable as Graph;
             plantUmlNodes = mapNodesToPlantUmlNodes(graphVariable.nodes);
             break;
         }
         case VariableType.Context: {
-            let contextVariable = variable as Context;
+            const contextVariable = variable as Context;
             plantUmlNodes = mapNodesToPlantUmlNodes(contextVariable.graph.nodes);
-            plantUmlNodes[contextVariable.contextNodeIndex].isBold = true;
+            plantUmlNodes[contextVariable.contextNodeIndex].border = "#569CD6";
+            plantUmlNodes[contextVariable.contextNodeIndex].background = "#264F78";
 
             break;
         }
         case VariableType.Pattern: {
-            let patternVariable = variable as Pattern;
+            const patternVariable = variable as Pattern;
             plantUmlNodes = mapNodesToPlantUmlNodes(patternVariable.graph.nodes);
-                        
-            let matchIndex = patternVariable.operations.keys.indexOf('MATCH');
-            let matchOperations: MatchOperationItem[] = [];
-            if (matchIndex >= 0) {
-                matchOperations = patternVariable.operations.values[matchIndex] as MatchOperationItem[];
-            }
+        
+            const matchOperations = getOperationByType<MatchOperationItem>( patternVariable.operations, 'MATCH');
+            const returnOperations = getOperationByType<ReturnOperationItem>( patternVariable.operations, 'RETURN');
+            const createOperations = getOperationByType<CreateOperationItem>( patternVariable.operations, 'CREATE');
+            const deleteOperations = getOperationByType<DeleteOperationItem>( patternVariable.operations, 'DELETE');
 
-            let returnIndex = patternVariable.operations.keys.indexOf('RETURN');
-            let returnOperations: ReturnOperationItem[] = [];
-            if (returnIndex >= 0) {
-                returnOperations = patternVariable.operations.values[returnIndex] as ReturnOperationItem[];
-            }
-
-            let createIndex = patternVariable.operations.keys.indexOf('CREATE');
-            let createOperations: CreateOperationItem[] = [];
-            if (createIndex >= 0) {
-                createOperations = patternVariable.operations.values[createIndex] as CreateOperationItem[];
-            }
-
-            let deleteIndex = patternVariable.operations.keys.indexOf('DELETE');
-            let deleteOperations: DeleteOperationItem[] = [];
-            if (deleteIndex >= 0) {
-                deleteOperations = patternVariable.operations.values[deleteIndex] as DeleteOperationItem[];
-            }
-            
-            matchOperations.forEach((matchOperation, index) => {
-                if (!isNaN(matchOperation.nodeIndex as number)) {
-                    plantUmlNodes[matchOperation.nodeIndex as number].isBold = true;
-                } else {
-                    plantUmlNodes[(matchOperation.relationIndex as [number, number])[0]]
-                        .relations[(matchOperation.relationIndex as [number, number])[1]].isBold = true;
-                }
-            });
-
-            returnOperations.forEach((returnOperation, index) => {
-                if (!isNaN(returnOperation.nodeIndex as number)) {
-                    plantUmlNodes[returnOperation.nodeIndex as number].color = '#264F78';
-                } else {
-                    plantUmlNodes[(returnOperation.relationIndex as [number, number])[0]]
-                        .relations[(returnOperation.relationIndex as [number, number])[1]].color = '#264F78';
-                }
-            });
-
-            createOperations.forEach((createOperation, index) => {
-                if (!isNaN(createOperation.nodeIndex as number)) {
-                    plantUmlNodes[createOperation.nodeIndex as number].color = '#2AA8B0';
-                } else {
-                    plantUmlNodes[(createOperation.relationIndex as [number, number])[0]]
-                        .relations[(createOperation.relationIndex as [number, number])[1]].color = '#2AA8B0';
-                }
-            });
-
-            deleteOperations.forEach((deleteOperation, index) => {
-                if (!isNaN(deleteOperation.nodeIndex as number)) {
-                    plantUmlNodes[deleteOperation.nodeIndex as number].color = '#A586C0';
-                } else {
-                    plantUmlNodes[(deleteOperation.relationIndex as [number, number])[0]]
-                        .relations[(deleteOperation.relationIndex as [number, number])[1]].color = '#A586C0';
-                }
-            });
+            setColorsOfElements(plantUmlNodes, matchOperations, '#9CDCFE', '#004E4E');
+            setColorsOfElements(plantUmlNodes, returnOperations, '#569CD6', '#264F78');
+            setColorsOfElements(plantUmlNodes, createOperations, '#2AA8B0');
+            setColorsOfElements(plantUmlNodes, deleteOperations, '#A586C0');
 
             break;
         }
@@ -91,6 +42,30 @@ export function generatePlantUmlScript(variableType: VariableType, variable: any
     }
 
     return generateScript(plantUmlNodes);
+}
+
+function getOperationByType<T>(operations: { keys: Operations[]; values: OperationItem[][]; }, key: Operations): T[] {
+    let index = operations.keys.indexOf(key);
+    if (index < 0) {
+        return [];
+    }
+
+    return operations.values[index] as T[];
+}
+
+function setColorsOfElements(elements: PlantUmlNode[], opertaions: OperationItem[], borderColor: string, backgroundColor?: string) {
+    opertaions.forEach(operation => {
+        if (!isNaN(operation.nodeIndex as number)) {
+            elements[operation.nodeIndex as number].border = borderColor;
+            elements[operation.nodeIndex as number].background = backgroundColor;
+        } else {
+            const relationOperation = operation as RelationIndex;
+            elements[(relationOperation.relationIndex as [number, number])[0]]
+                .relations[(relationOperation.relationIndex as [number, number])[1]].border = borderColor;
+                elements[(relationOperation.relationIndex as [number, number])[0]]
+                .relations[(relationOperation.relationIndex as [number, number])[1]].background = backgroundColor;
+        }
+    });
 }
 
 function mapNodesToPlantUmlNodes(nodes: GraphNode[]): PlantUmlNode[] {
@@ -113,22 +88,21 @@ function mapNodesToPlantUmlNodes(nodes: GraphNode[]): PlantUmlNode[] {
 function generateScript(nodes: PlantUmlNode[]): string {
 
     let labelAttribute = (label: string, index: string) => `label=<${label === 'null' ? `<FONT color="#569CD6">${label}</FONT>` : label} <SUB><FONT color="#B5CEA8" face="times">[${index}]</FONT></SUB>>`;
-    let boldAttributes = (isBold: boolean | undefined) => isBold ? ',penwidth="3",fontname="bold"' : '';
-    let colorAttributes = (color: string | undefined) => color ? `,style="filled",fillcolor="${color}",pencolor="${color}",color="${color}"` : '';
+    let colorAttributes = (border?: string, background?: string) => (border ? `pencolor="${border}",color="${border}"` : '') + (background ? `,style="filled",fillcolor="${background}"` : '');
 
     return `
         @startuml
         digraph graph1 {
             graph [ fontsize=9, bgcolor="transparent" ];
-            node [ fontsize=9, fontcolor="#CE9178" color="#CCCCCC" ];
-            edge [ fontsize=9, fontcolor="#CE9178" color="#CCCCCC" ];
+            node [ penwidth=2, fontsize=9, fontcolor="#CE9178" color="#CCCCCC" ];
+            edge [ penwidth=2, fontsize=9, fontcolor="#CE9178" color="#CCCCCC" ];
 
             ${nodes.map(node => {
-                return `${node.name} [${labelAttribute(node.label, node.index.toString())}${boldAttributes(node.isBold)}${colorAttributes(node.color)}]`;
+                return `${node.name} [${labelAttribute(node.label, node.index.toString())},${colorAttributes(node.border, node.background)}]`;
             }).join('\n')}
 
             ${nodes.map(node => node.relations.map(relation => {
-                return `${node.name}->${nodes[relation.targetNodeIndex].name} [${labelAttribute(relation.name, node.index + ', ' + relation.index)}${boldAttributes(relation.isBold)}${colorAttributes(relation.color)}]`;
+                return `${node.name}->${nodes[relation.targetNodeIndex].name} [${labelAttribute(relation.name, node.index + ', ' + relation.index)},${colorAttributes(relation.border)}]`;
             }).join('\n')).join('\n')}
         }
         @enduml`;
